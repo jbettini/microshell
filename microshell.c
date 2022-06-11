14866d72af427d4fd4e7863179f3d5c634c09831
bettini joseph <ashhxtm@gmail.com>
2022-06-11 03:00:57 +0200

    LastSave

---
+++ microshell.c
@@ -0,0 +1,148 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+# define ERR_CD_ARG "error: cd: bad arguments\n"
+# define ERR_CD_FATAL "error: cd: cannot change directory to"
+# define ERR_FATAL "error: fatal\n"
+# define ERR_CMD "error: cannot execute "
+
+void    put_strs(char **arg)
+{
+    int i = 0;
+
+    while (arg[i])
+        printf("%s\n", arg[i++]);
+}
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return (i);
+}
+
+char *ft_strdup(char *str)
+{
+    char    *new;
+    int     i;
+
+    i = -1;
+    new = malloc(1 * ft_strlen(str) + 1);
+    while (str[++i])
+        new[i] = str[i];
+    new[i] = 0;
+    return new;
+}
+
+void    ft_free_strs(char **strs)
+{
+    int i;
+
+    i = 0;
+    while (strs[i])
+        free(strs[i++]);
+    free(strs);
+}
+
+int put_error(char *msg, char *path)
+{
+    write(2, msg, ft_strlen(msg));
+    if (path)
+    {
+        write(2, path, ft_strlen(path));
+        write(2, "\n", 1);
+    }
+    return (1);
+}
+
+int ft_cd(char **arg)
+{
+    if (arg[2])
+        return (put_error(ERR_CD_ARG, NULL));
+    else if (chdir(arg[1]) == -1)
+        return (put_error(ERR_CD_FATAL, arg[1]));
+    return 0;
+}
+
+
+char    **get_word(char **arg, char *token)
+{
+    char    **pipeline;
+    int     i;
+
+    i = 0;
+    while (arg[i] && strcmp(arg[i], token))
+        i++;
+    pipeline = malloc(8 * (i + 1));
+    i = -1;
+    while (arg[++i] && strcmp(arg[i], token))
+        pipeline[i] = ft_strdup(arg[i]);
+    pipeline[i] = 0;
+    return (pipeline);
+}
+
+int next_token(char **arg, char *token)
+{
+    int     i;
+
+    i = 0;
+    while (arg[i] && strcmp(arg[i], token))
+        i++;
+    if (!arg[i])
+        return (i);
+    return (++i);
+}
+
+int pipex(char **arg, char **envp, int infile)
+{
+    
+}
+
+void    exec_cmd(char **arg, char **envp)
+{
+    int fd_in;
+    int i;
+    char **cmd;
+
+    i = 0;
+    fd_in = STDIN_FILENO;
+    while (arg[i])
+    {
+        cmd = get_word(&arg[i], "|");
+        if (!strcmp(cmd[0], "cd"))
+        {
+            ft_cd(cmd);
+            ft_free_strs(cmd);
+            return ;
+        }
+        i += next_token(&arg[i], "|");
+        if (arg[i])
+            fd_in = pipex(cmd, envp, fd_in);
+        else
+            exec_last(cmd, envp, fd_in);
+        ft_free_strs(cmd);
+    }
+}
+
+int main(int ac, char **av, char **envp)
+{
+    char    **pipeline;
+    int i = 1;
+
+    while (i < ac)
+    {
+        pipeline = get_word(&av[i], ";");
+        exec_cmd(pipeline, envp);
+        i += next_token(&av[i], ";");
+        ft_free_strs(pipeline);
+    }
+    // system("leaks a.out");
+    return 0;
+}
